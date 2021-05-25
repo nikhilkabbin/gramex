ec878e623837e1e484d139bb5d038a89569c94e1
S Anand <root.node@gmail.com>
2021-05-25 08:59:14 +0530

    WIP

--- gramex/apps/ui/__init__.py
+++ gramex/apps/ui/__init__.py
@@ -149,24 +149,22 @@ def sass2(handler, path: str = join(ui_dir, 'gramexui.scss')):
 
 @coroutine
 def vue(handler, path: str):
-    '''
-    Compile a .vue file
-    '''
+    '''Compile a .vue file'''
     # Get valid variables from URL query parameters
     # Create cache key based on state = path. Output to <cache-key>.js
     path = os.path.normpath(path).replace('\\', '/')
     cache_key = get_cache_key([path])
-    vue_dir = join(cache_dir, f'vue-{cache_key}')
+    target_dir = join(cache_dir, f'vue-{cache_key}')
 
     # Recompile if output cache_file is missing, or path has been updated
-    if not os.path.exists(vue_dir) or os.stat(path).st_mtime > os.stat(vue_dir).st_mtime:
+    if not os.path.exists(target_dir) or os.stat(path).st_mtime > os.stat(target_dir).st_mtime:
         # Compile Vue file
         cwd, filename = os.path.split(path)
 
         proc = yield gramex.service.threadpool.submit(subprocess.run, [
             # unhandled-rejections ensures that returncode != 0 on error
             'node', '--unhandled-rejections=strict',
-            vue_path, 'build', '--target', 'wc', filename, '--dest', vue_dir
+            vue_path, 'build', '--target', 'wc', filename, '--dest', target_dir
         ], cwd=cwd, capture_output=True, encoding='utf-8')
         if proc.returncode:
             raise RuntimeError('Vue compilation failure', proc.stdout)
@@ -176,11 +174,11 @@ def vue(handler, path: str):
     if 'map' in handler.args:
         # Serve map file if browser requested component-name.vue?map
         handler.set_header('Content-Type', 'application/json')
-        return gramex.cache.open(os.path.join(vue_dir, target + '.map'), 'bin', mode='rb')
+        return gramex.cache.open(os.path.join(target_dir, target + '.map'), 'bin', mode='rb')
     else:
         # Serve compiled JS if browser requested just .vue
         handler.set_header('Content-Type', 'text/javascript')
-        content = gramex.cache.open(os.path.join(vue_dir, target), 'bin', mode='rb')
+        content = gramex.cache.open(os.path.join(target_dir, target), 'bin', mode='rb')
         # ... but replace the map file with component-name.vue?map
         return content.replace(
             f'//# sourceMappingURL={target}.map'.encode('utf-8'),
